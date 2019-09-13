b9b8350a6dac70adb9d5004d1ae8e6ace3d8988d
Brianna <bs3864@nyu.edu>
2019-09-13 09:40:19 -0500

    Wanted Poster Added
    Added a wanted poster that appears and will spawn cowboys who leave coins after they leave the scene.

---
+++ W1_Shuttleworth/Assets/Scripts/WantedPoster.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WantedPoster : MonoBehaviour
+{
+
+     GameObject wantedPoster;
+    public GameObject posterPrompt, CurrencyManager;
+    
+    public List<GameObject> Cowboys;
+    private Vector3 cowboyLocation;
+    private float groundLeft, groundRight;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       CurrencyManager = GameObject.Find("CurrencyManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (wantedPoster == null)
+        {
+            posterPrompt.SetActive(true);
+        }
+
+    }
+
+    public void WantedPosterOn()
+    {
+        if (CurrencyManager.GetComponent<FreemiumCurrency>().coin >= 10)
+        {
+            if (wantedPoster == null)
+            {
+                wantedPoster = Instantiate(Resources.Load("Prefabs/WantedPoster")) as GameObject;
+                CurrencyManager.GetComponent<FreemiumCurrency>().BasicPosterCost();
+            }
+        }
+
+        //start coroutine for how long the poster runs and will stay up.
+        StartCoroutine(PosterDecay(5f));
+    }
+
+    public void turnOffPrompt()
+    {
+        posterPrompt.SetActive(false);
+    }
+
+    IEnumerator PosterDecay(float time)
+    {
+        //Vector3 cowboyLocation = new Vector3(Random.Range(0,30),0,Random.Range(-1,5));
+        for (int i=0; i<=10; i += Random.Range(1,3))
+        {
+            Vector3 cowboyLocation = new Vector3(Random.Range(-6,8),0,Random.Range(-5,3));
+            Cowboys.Add(Instantiate(Resources.Load("Prefabs/Cowboy"),cowboyLocation, Quaternion.identity) as GameObject);
+            
+        }  
+        yield return new WaitForSeconds(time);
+        Destroy(wantedPoster);
+    }
+
+
+        
+}
